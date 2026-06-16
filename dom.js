e528e276ddc7be3a7b53ba94cdc605ea3d09ea0e
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-06-16 10:51:12 +0530

    Your descriptive commit message

---
+++ dom.js
@@ -0,0 +1,16 @@
+//document.getElementById("heading");
+//document.getElementsByClassName("heading");
+//document.getElementsByTagName("h1");
+
+//document.querySelector("h1");
+//document.querySelector("#heading");
+//document.querySelector(".heading");
+
+//document.querySelectorAll("h1");//
+
+function ChangeText(){
+    let heading = document.querySelector("#heading");
+    heading.innerText = "WELCOME";
+    heading.style.color = "Red";
+    document.body.bgColor = "yellow";
+}
